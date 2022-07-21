380f60f0704decaaae27056723788051fbedb069
Nadia <nadiadunaeva@gmail.com>
2022-07-21 18:38:34 +0200

    Feat: new pages

---
+++ script/test.js
@@ -0,0 +1,34 @@
+/* Explore more articles dropdown */
+
+window.onload=function() {
+    document.getElementById("add_block_1").style.display="none";
+    document.getElementById("add_block_2").style.display="none";
+    document.getElementById("add_block_3").style.display="none";
+    document.getElementById("toggle").onclick=function() {
+      var divs = this.getAttribute("data-divs").split(",");
+     
+      for(var i = 0,len = divs.length;i < len;i++){
+        var e = document.getElementById(divs[i]).style;
+        console.log(e.display)
+        e.display = (e.display == "") ? "none" : "";
+      }
+      return false;
+    }
+  }  
+/* Filer By dropdown */
+  function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  // Close the dropdown 
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
